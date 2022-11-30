72625d0660289cfce2a0f0ed9d89a198e7484648
Kligunov <kligunov.id@phystech.edu>
2022-11-30 13:24:08 +0300

    Set up SDL2 on Linux

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "SDL.h"
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, world!\n";
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Wires",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
